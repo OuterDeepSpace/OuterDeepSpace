2ebd3775eee951f68eb31d05b40606495d6f9e14
anderuso <anderuso@9ec6d687-074c-4f17-8882-0daec8a15dc2>
2006-01-26 22:30:56 +0000

    Added setup files for creating source distribution
    git-svn-id: svn://svn.code.sf.net/p/ospace/code/trunk@14 9ec6d687-074c-4f17-8882-0daec8a15dc2

---
+++ ospace/server/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup(name='OuterSpaceServer-src',
+      version='0.5.54',
+      description='OuterSpace Server',
+      author='Qark',
+      author_email='qark@ospace.net',
+      url='http://www.sourceforge.net/projects/ospace',
+      py_modules = ['run', 'osclient', 'useradd', 'prof', 'profres', 'lib.log'],
+      packages=['lib.ige', 'lib.igeclient', 'lib.medusa', 'lib.ige.ospace', 'lib.ige.ospace.Rules'],
+     )
