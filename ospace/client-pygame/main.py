40f15feb1c695e4447b1be298caf227600058315
qark <qark@9ec6d687-074c-4f17-8882-0daec8a15dc2>
2007-05-05 05:16:18 +0000

    - Fixed few typos
    git-svn-id: svn://svn.code.sf.net/p/ospace/code/trunk@121 9ec6d687-074c-4f17-8882-0daec8a15dc2

--- ospace/client-pygame/main.py
+++ ospace/client-pygame/main.py
@@ -1,14 +1,14 @@
 #
 #  Copyright 2001 - 2006 Ludek Smid [http://www.ospace.net/]
 #
-#  This file is part of IGE - Outer Space.
+#  This file is part of Outer Space.
 #
-#  IGE - Outer Space is free software; you can redistribute it and/or modify
+#  Outer Space is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  IGE - Outer Space is distributed in the hope that it will be useful,
+#  Outer Space is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
